a089b2e85c2c04930efcb6df9fa05561a49aaa79
abd-ulbasit <basit01035@gmail.com>
2022-09-27 19:27:12 +0500

    using * in cors

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(
     cors({
-        origin: ["http://localhost:5173"],
+        origin: "*",
     })
 );
 const Todo = require("./models/Todo");
